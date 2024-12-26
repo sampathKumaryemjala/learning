d1083c02a93a410398ef7c8460c79032827fd412
Sampath Kumar Yemjala <sampath.yemjala@compileinfy.com>
2024-12-26 17:13:28 +0530

    This is a new

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read Sampath..
+            Read Sampath...
           </a>
         </div>
       </main>
